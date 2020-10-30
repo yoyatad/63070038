3f5fc6adc80001ea1e6dd04a8b08343111d7b6df
yoyatad <73683138+yoyatad@users.noreply.github.com>
2020-10-30 12:32:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+  <head>
+    <title>YOYATAD Group</title>
+  </head>
+  <body>
+    <h1>My First Heading</h1>
+    <p>MY First Paragraph</p>p>
+    <img src="cat.jpg">
+    <br>
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h2>The unordered list</h2>
+    <ul>
+        <li>Google</li>
+        <li>Yahoo</li>
+        <li>Google</li>
+    </ul>
+
+  </body>
+</html>
