41085602a9d1fd3d43d108beac9bb0c2989cea31
yoyatad <73683138+yoyatad@users.noreply.github.com>
2020-11-19 22:09:34 +0700

    Add files via upload

--- ITFLabsss/edit.php
+++ ITFLabsss/edit.php
@@ -17,43 +17,39 @@
 <head>
 	<title>แก้ไขฟอร์ม</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-	<style type="text/css">
-		.card {
-			border-top: solid 5px #ffc107;
-		}
-	</style>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>>
 </head>
-<body class="bg-light py-5">
+<body>
 	<div class="container text-dark">
-		<div class="row">
-			<div class="col-12 col-lg-8 offset-lg-2">
-				<div class="card shadow">
-					<div class="card-body">
-						<div class="row">
-							<div class="col-6"><h1 class="text-monospace">Edit</h1></div>
-						</div>
-						<form action="update.php" method="post">
-							<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
-							<div class="form-group">
-								<label>Name</label>
-								<input type="text" name="name" value="<?php echo $data['Name']; ?>" class="form-control" required>
-							</div>
-							<div class="form-group">
-								<label>Comment</label>
-								<textarea name="comment" class="form-control" rows="5" required><?php echo $data['Comment']; ?></textarea>
-							</div>
-							<div class="form-group">
-								<label>Link</label>
-								<input type="text" name="link" value="<?php echo $data['Link']; ?>" class="form-control">
-							</div>
-							<div class="text-center">
-								<button type="submit" class="btn btn-sm btn-warning my-3 font-weight-bold">SAVE</button>
-								<a class="btn btn-sm btn-light my-3" href="index.php">CANCLE</a>
-							</div>
-						</form>
+		<div class="col-12 col-lg-8 offset-lg-2">
+			<div class="card shadow">
+				<div class="card-body">
+					<div class="row">
+						<div class="col-6"><h1 class="text-monospace">แก้ไขข้อมูล</h1></div>
 					</div>
+					<form action="update.php" method="post">
+						<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
+						<div class="form-group">
+							<label>ชื่อ</label>
+							<input type="text" name="name" value="<?php echo $data['Name']; ?>" class="form-control" placeholder=">>>แก้อะไรดีน้าาาาาาาาาา>>>" required>
+						</div>
+						<div class="form-group">
+							<label>คำอธิบาย</label>
+							<input name="comment" class="form-control" rows="5" placeholder=">>>เขียนมาเถอะไม่ต้องคิดมาก>>>" required><?php echo $data['Comment']; ?>
+						</div>
+						<div class="form-group">
+							<label>ที่อยู่เว็บไซต์</label>
+							<input type="text" name="link" value="<?php echo $data['Link']; ?>" class="form-control" placeholder=">>>ฮั่นแน่!!!!!!!!!!!!!>>>">
+						</div>
+						<div class="text-center">
+							<button type="submit" class="btn btn-success">บันทึกข้อมูล</button>
+							<a class="btn btn-danger" href="index.php">ยกเลิกการแก้ไข</a>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
