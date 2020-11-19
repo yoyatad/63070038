0d27fc9f9fdec2c8643adf71c734137fdaaabe56
yoyatad <73683138+yoyatad@users.noreply.github.com>
2020-11-19 22:00:08 +0700

    Add files via upload

--- ITFLabsss/update.php
+++ ITFLabsss/update.php
@@ -13,32 +13,28 @@
 <head>
 	<title>อัพเดตข้อมูล</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-	<style type="text/css">
-		.card {
-			border-top: solid 5px #ffc107;
-		}
-	</style>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>>
 </head>
-<body class="bg-light py-5">
+<body>
 	<div class="container text-dark">
-		<div class="row">
-			<div class="col-12 col-lg-8 offset-lg-2">
-				<div class="card shadow">
-					<div class="card-body">
-						<h2 align="center">
-						<?php
-							if(mysqli_query($conn, $sql)) {
-								echo "EDIT COMPLETED";
-							}
-							else {
-								echo "FAILED TO EDIT";
-							}
-						?>
-						</h2>
-						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-sm btn-warning">BACK</a></p>
-					</div>
+		<div class="col-12 col-lg-8 offset-lg-2">
+			<div class="card shadow">
+				<div class="card-body">
+					<h2 align="center">
+					<?php
+						if(mysqli_query($conn, $sql)) {
+								echo "แก้ไขข้อมูลสำเร็จ";
+						}
+						else {
+							echo "แก้ไขข้อมูลล้มเหลว";
+						}
+					?>
+					</h2>
+					<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-info">กลับสู่หน้าหลัก</a></p>
 				</div>
 			</div>
 		</div>
