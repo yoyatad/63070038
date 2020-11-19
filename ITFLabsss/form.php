5a61bfb3fdd693e34216d2d1c9a6f10443d01843
yoyatad <73683138+yoyatad@users.noreply.github.com>
2020-11-19 23:12:28 +0700

    Add files via upload

--- ITFLabsss/form.php
+++ ITFLabsss/form.php
@@ -10,40 +10,40 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body class="bg-dark">
-<div class="container">
-	<div class="card shadow">
-		<div class="col-12 col-lg-8 offset-lg-2">
-			<div class="card-body">
-				<h2>กรุณากรอกข้อมูลของท่าน</h2>
-				<p>โปรดกรอกข้อมูลลงแบบฟอร์มของเราซะ!!! ไม่มีอะไรน่าสงสัยหรอกน้าาาาา หยอกเย่นๆๆๆๆๆ</p>
-				<form action = "insert.php" action="/action_page.php" method = "post" id="CommentForm" class="was-validated">
-					<div class="form-group">
-					<label for="uname">ชื่อ:</label><br>
-					<input type="text" class="form-control" id="idName" placeholder=">>>ชื่อจริง ชื่อเล่น หรือ นามแฝงสุดเท่ของคุณ>>>" name="name" required>
-					<div class="valid-feedback">สวย</div>
-					<div class="invalid-feedback">ใส่ก่อน...จะรีบไปไหน</div>
-					</div>
-					<div class="form-group">
-					<label for="uname">คำอธิบายเกี่ยวกับตัวคุณ:</label><br>
-					<input type="text" class="form-control" id="idComment" placeholder=">>>เขียนมาเถอะ จุ๊บๆ>>>" name="comment" required>
-					<div class="valid-feedback">มันต้องอย่างงี้ดิ</div>
-					<div class="invalid-feedback">แหนะๆๆ..อย่าเพิ่งมองข้ามสิ</div>
-					</div>
-					<div class="form-group">
-					<label for="uname">ที่อยู่ของเว็บไซต์ที่ชื่นชอบ:</label><br>
-					<input type="text" class="form-control" id="idLink" placeholder=">>>ใส่ U r l อะไรก็ได้ที่คุณอยากใส่>>>" name="link" required>
-					<div class="valid-feedback">ก็มาดิค้าบบบบ</div>
-					<div class="invalid-feedback">มองข้ามหนูทำไมมมมT_T</div>
-					</div>
-					<h2 align="center">
-						<button type="submit" class="btn btn-success">!!!ส่งแบบฟอร์ม คลิกที่นี่!!!</button>
-					</h2>
-				</form>
 	<div class="container">
-		<div class="col-12 col-lg-8 offset-lg-2">
-			<h2 align="center">
-				<a href="index.php">
-    			<button type="submit" class="btn btn-info">ไปที่หน้าหลัก</button>
+		<div class="card shadow">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card-body">
+					<h2>กรุณากรอกข้อมูลของท่าน</h2>
+					<p>โปรดกรอกข้อมูลลงแบบฟอร์มของเราซะ!!! ไม่มีอะไรน่าสงสัยหรอกน้าาาาา หยอกเย่นๆๆๆๆๆ</p>
+					<form action = "insert.php" action="/action_page.php" method = "post" id="CommentForm" class="was-validated">
+						<div class="form-group">
+						<label for="uname">ชื่อ:</label><br>
+						<input type="text" class="form-control" id="idName" placeholder=">>>ชื่อจริง ชื่อเล่น หรือ นามแฝงสุดเท่ของคุณ>>>" name="name" required>
+						<div class="valid-feedback">สวย</div>
+						<div class="invalid-feedback">ใส่ก่อน...จะรีบไปไหน</div>
+						</div>
+						<div class="form-group">
+						<label for="uname">คำอธิบายเกี่ยวกับตัวคุณ:</label><br>
+						<input type="text" class="form-control" id="idComment" placeholder=">>>เขียนมาเถอะ จุ๊บๆ>>>" name="comment" required>
+						<div class="valid-feedback">มันต้องอย่างงี้ดิ</div>
+						<div class="invalid-feedback">แหนะๆๆ..อย่าเพิ่งมองข้ามสิ</div>
+						</div>
+						<div class="form-group">
+						<label for="uname">ที่อยู่ของเว็บไซต์ที่ชื่นชอบ:</label><br>
+						<input type="text" class="form-control" id="idLink" placeholder=">>>ใส่ U r l อะไรก็ได้ที่คุณอยากใส่>>>" name="link" required>
+						<div class="valid-feedback">ก็มาดิค้าบบบบ</div>
+						<div class="invalid-feedback">มองข้ามหนูทำไมมมมT_T</div>
+						</div>
+						<h2 align="center">
+							<button type="submit" class="btn btn-success">!!!ส่งแบบฟอร์ม คลิกที่นี่!!!</button>
+						</h2>
+					</form>
+		<div class="container">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<h2 align="center">
+					<a href="index.php">
+					<button type="submit" class="btn btn-info">ไปที่หน้าหลัก</button>
 </body>
 </html>
 </div>
